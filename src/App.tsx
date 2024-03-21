f96dbe57e2476c8ad7bde8f87710f73d1f472885
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-03-20 23:09:37 -0300

    chore: initial project config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/Header"
+
+export function App() {
+  return (
+    <main>
+      <Header />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="bg-gray-200 py-4 md:py-6 flex items-center justify-center">
+      <h1>Rock-Paper-Scissors GAME</h1>
+    </header>
+  )
+}
