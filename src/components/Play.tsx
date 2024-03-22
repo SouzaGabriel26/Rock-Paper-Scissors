f0bfaafc282d62a1de44fff121795ed631d6d732
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-03-22 20:19:40 -0300

    style: add style mainly to `play.tsx` component

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,6 +1,12 @@
 import { FormEvent, useState } from 'react';
 import { GameAction } from '../types/GameAction';
 import { GameOption } from '../types/GameOption';
+import { sleep } from '../utils/sleep';
+import { Option } from './Option';
+import { Spinner } from './Spinner';
+import { HandFist } from './icons/HandFirst';
+import { HandOpen } from './icons/HandOpen';
+import { HandScissors } from './icons/HandScissors';
 
 type PlayProps = {
   dispatchEvent: React.Dispatch<GameAction>;
@@ -8,13 +14,18 @@ type PlayProps = {
 
 export function Play({ dispatchEvent }: PlayProps) {
   const [playerChoice, setPlayerChoice] = useState<GameOption | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handlePlayerChoice(option: GameOption) {
     setPlayerChoice(option);
   }
 
-  function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
+  async function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    setIsLoading(true);
+    await sleep(1000);
+    setIsLoading(false);
 
     if (playerChoice) {
       dispatchEvent({ type: 'SET_PLAYER_CHOICE', option: playerChoice });
@@ -23,21 +34,35 @@ export function Play({ dispatchEvent }: PlayProps) {
 
   return (
     <div>
-      <div>
+      <div className="mb-6 text-2xl text-slate-800">
         <h2>Choose one:</h2>
       </div>
 
-      <form onSubmit={handleFormSubmit}>
-        <button type="submit" onClick={() => handlePlayerChoice('rock')}>
-          Rock
-        </button>
-        <button type="submit" onClick={() => handlePlayerChoice('paper')}>
-          Paper
-        </button>
-        <button type="submit" onClick={() => handlePlayerChoice('scissors')}>
-          Scissors
-        </button>
+      <form
+        onSubmit={handleFormSubmit}
+        className="flex items-center justify-center gap-4"
+      >
+        <Option
+          value="rock"
+          disabled={isLoading}
+          children={<HandFist />}
+          onPlayerChoice={handlePlayerChoice}
+        />
+        <Option
+          value="paper"
+          disabled={isLoading}
+          children={<HandOpen />}
+          onPlayerChoice={handlePlayerChoice}
+        />
+        <Option
+          value="scissors"
+          disabled={isLoading}
+          children={<HandScissors />}
+          onPlayerChoice={handlePlayerChoice}
+        />
       </form>
+
+      {isLoading && <Spinner />}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'show-content-down': {
+          '0%': {
+            transform: 'translateY(-100%)',
+            opacity: 0,
+            height: 0,
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: 1,
+            height: 'auto'
+          },
+        },
+      },
+      animation: {
+        'show-content-down': 'show-content-down 0.2s ease-in-out',
+      }
+    },
   },
   plugins: [],
 }
