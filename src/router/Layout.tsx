8c1c6a2cad95b6811a1da82119e916441c6a529d
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-03-26 21:57:43 -0300

    feat: implement rules-modal with ReactPortal

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 
 export function Layout() {
   return (
-    <div className="h-full">
+    <div className="flex h-full flex-col">
       <Header />
 
-      <main className="mx-auto mt-32 max-w-[600px]">
+      <main className="mx-auto mt-32 max-w-[600px] flex-grow">
         <Outlet />
       </main>
+
+      <Footer />
     </div>
   );
 }
